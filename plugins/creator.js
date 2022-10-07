f2bd94f35a8c7a1fd0db0b67131d8e5bd4136486
Aeull <110038479+Aeull@users.noreply.github.com>
2022-10-08 02:19:10 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,16 +3,7 @@ let handler = async(m, { conn }) => {
 	conn.p = conn.p ? conn.p : {}
 	let id = m.chat
 	conn.p[id] = [
-	await conn.sendKontak(m.chat, `${nomorown.split`@`[0]}`, fgif, { contextInfo: { externalAdReply :{
-    showAdAttribution: true,
-    mediaUrl: 'https://Instagram.com/sahrulwara_____',
-    mediaType: 2,
-    body: 'Owner Saya Ganteng Loh Kak',
-    title: 'Nih My Owner',
-    sourceUrl: sgc,
-    thumbnail: await(await fetch('https://telegra.ph/file/8b8dfb14adc5ccc25d4ae.jpg')).buffer(),
-     }}
-  })
+	await conn.sendContact(m.chat, nomorown, nameown, fgif)
 	]
 	await delay(100)
   return conn.sendMessage(m.chat, { text: `Hay kak @${m.sender.split('@')[0]}, itu nomor ownerku jangan dispam yah ^_^`, mentions: [m.sender] }, { quoted: conn.p[id][0]
