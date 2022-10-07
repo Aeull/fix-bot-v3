a306d914f59ab900a7b5f45f7b69bc80b514f7d7
Aeull <110038479+Aeull@users.noreply.github.com>
2022-10-08 02:24:43 +0800

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 import { tiktok } from "social_media_downloader"
 let handler = async (m, { conn, usedPrefix, command, text, args }) => {
-if (!args[0]) throw 'Masukkan Link'
+if (!args[0]) throw 'Linknya Mana?'
 try {
 let p = await tiktok(args[0])
     if (!p.link) throw 'Can\'t download video!'
@@ -22,7 +22,7 @@ let p = await tiktok(args[0])
 
 _© ArullBotz_
 `.trim()
-conn.send2ButtonVid(m.chat, p.link, cap, author, `Donasi`, `.donasi`, `Audio`, `.tta ${args[0]}`, fakes, adReply)
+conn.send2ButtonVid(m.chat, p.link, cap, author, `Donasi`, `.donasi`, `Audio`, `.tta ${args[0]}`, fgif)
 } catch (e) {
     throw eror
     }
