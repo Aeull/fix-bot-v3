edca65a95425a6c3ef80508b27f8a7ec6d778bff
Aeull <110038479+Aeull@users.noreply.github.com>
2022-10-07 21:50:11 +0800

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -8,7 +8,7 @@ await conn.send2ButtonImg(m.chat, thumb, info, '⌕ ❙❘❙❙❘❙❚❙❘�
       mediaUrl: 'https://instagram.com/sahrulwara_____',
       mediaType: 2,
       Title: `Hallo Kak ${conn.getName(m.sender)}`,
-      thumbnail: await(await fetch(pp)).buffer(),
+      thumbnail: await(await fetch(img)).buffer(),
        }}})
 await conn.sendMessage(m.chat, {
           react: {
