e415f5ceed833781059148e67cbe057590485374
Aeull <110038479+Aeull@users.noreply.github.com>
2022-10-07 21:58:57 +0800

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -7,7 +7,7 @@ await conn.send2ButtonImg(m.chat, thumb, info, '⌕ ❙❘❙❙❘❙❚❙❘�
       showAdAttribution: true,
       mediaUrl: 'https://instagram.com/sahrulwara_____',
       mediaType: 2,
-      Title: `Hallo Kak ${conn.getName(m.sender)}`,
+      title: `Hallo Kak ${conn.getName(m.sender)}`,
       thumbnail: await(await fetch(`https://telegra.ph/file/6840494256f74f31a0bfa.jpg`)).buffer(),
        }}})
 await conn.sendMessage(m.chat, {
