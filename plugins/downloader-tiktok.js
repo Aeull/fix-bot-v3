4b2eb60b128d0a8474deee62951aaef0db12144f
Aeull <110038479+Aeull@users.noreply.github.com>
2022-10-08 23:42:54 +0800

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -45,7 +45,7 @@ export default handler
 
 import fetch from 'node-fetch'
 import axios from 'axios'
-import { tiktok } from "https://allinonedownloader.com/tiktok-video-downloader.php"
+import { tiktok } from "savefrom"
 let handler = async (m, { conn, usedPrefix, command, text, args }) => {
 if (!args[0]) throw 'Masukkan Link'
 try {
